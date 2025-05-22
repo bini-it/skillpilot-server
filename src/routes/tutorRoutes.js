c4707cdc25bd6415a178c374f5a1a94e934e2b65
bini-it <143047673+bini-it@users.noreply.github.com>
2025-05-22 06:16:45 +0300

    Delete src/routes directory

--- src/routes/tutorRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import verifyTokenFromQuery from '../middleware/verifyTokenFromQuery.js';
-import { streamAiTutorReply } from '../controllers/tutorController.js';
-
-const router = express.Router();
-
-router.get('/', verifyTokenFromQuery , streamAiTutorReply);
-
-export default router;
