59dff98c23a14a12a7cfe640893a2e906d695bbc
bini-it <143047673+bini-it@users.noreply.github.com>
2025-05-26 05:56:46 +0300

    Delete src directory

--- src/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getUserNotifications } from '../controllers/notificationController.js';
-import authenticateUser from '../middleware/authenticateUser.js';
-
-const router = express.Router();
-
-router.get('/:userId', authenticateUser, getUserNotifications);
-
-export default router;
